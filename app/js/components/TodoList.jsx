39d9bc1e58a79409e3652359cc656ac52f1fa995
Klas Mellbourn <klas@mellbourn.net>
2018-01-01 22:03:38 +0100

    refactor to remove all eslint errors

--- app/js/components/TodoList.jsx
+++ app/js/components/TodoList.jsx
@@ -1,84 +1,63 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { StatelessComponent } from "react"; // eslint-disable-line no-unused-vars
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-import { toggleTodo } from '../actions';
+import { toggleTodo } from "../actions";
 
-const Todo = ({
-  onClick,
-  completed,
-  text
-  }) => (
+/** @type { StatelessComponent<{onClick, completed, text}> } */
+const Todo = ({ onClick, completed, text }) => (
   <li
     onClick={onClick}
     style={{
-      textDecoration:
-        completed ?
-          'line-through' :
-          'none'
+      textDecoration: completed ? "line-through" : "none"
     }}
-    className={
-        completed ?
-          'completed' :
-          ''
-    }
-    >
+    className={completed ? "completed" : ""}
+  >
     {text}
   </li>
 );
 
-const TodoList = ({
-  todos,
-  onTodoClick
-  }) => (
+Todo.propTypes = {
+  onClick: PropTypes.func,
+  completed: PropTypes.func,
+  text: PropTypes.string
+};
+
+/** @type { StatelessComponent<{todos, onTodoClick}> } */
+const TodoList = ({ todos, onTodoClick }) => (
   <ul>
-    {todos.map(todo =>
-        <Todo
-          key={todo.id}
-          {...todo}
-          onClick={() => onTodoClick(todo.id)}
-          />
-    )}
+    {todos.map(todo => (
+      <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+    ))}
   </ul>
 );
 
-const getVisibleTodos = (
-  todos,
-  filter
-) => {
-  switch (filter) {
-    case 'SHOW_ALL':
-      return todos;
-    case 'SHOW_COMPLETED':
-      return todos.filter(
-          t => t.completed
-      );
-    case 'SHOW_ACTIVE':
-      return todos.filter(
-          t => !t.completed
-      );
-  }
-}
+TodoList.propTypes = {
+  todos: PropTypes.array,
+  onTodoClick: PropTypes.func
+};
 
-const mapStateToProps = (
-  state
-) => {
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case "SHOW_ALL":
+      return todos;
+    case "SHOW_COMPLETED":
+      return todos.filter(t => t.completed);
+    case "SHOW_ACTIVE":
+      return todos.filter(t => !t.completed);
+  }
+};
+
+const mapStateToProps = state => {
   return {
-    todos: getVisibleTodos(
-      state.todos,
-      state.visibilityFilter
-    )
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
   };
 };
-const mapDispatchToProps = (
-  dispatch
-) => {
+const mapDispatchToProps = dispatch => {
   return {
-    onTodoClick: (id) => {
+    onTodoClick: id => {
       dispatch(toggleTodo(id));
     }
   };
 };
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoList);
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
