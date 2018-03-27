6a92fad26e4b40583b89a7091dbf3a6ca8a671e9
Pavel Zinin <pzinin@gmail.com>
2018-03-27 22:17:29 +0300

    Replace my example with slightly modified Dan example

---
+++ src/components/Root.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import {BrowserRouter as Router, Route, browserHistory } from 'react-router-dom';
+import App from './App';
+const Root = ({ store }) => (
+    <Provider store={store}>
+        <Router /*history={browserHistory}*/>
+            <Route path='/' component={App} />
+        </Router>
+    </Provider>
+);
+
+Root.propTypes = {
+    store: PropTypes.object.isRequired,
+};
+
+export default Root;
---
+++ src/configureStore.js
@@ -0,0 +1,24 @@
+import {loadState, saveState} from "./localStorage";
+import {createStore } from 'redux';
+
+import throttle from 'lodash/throttle';
+import todoApp from './reducers';
+
+
+const configureStore = () => {
+    const persistedState = loadState();
+    const store = createStore(
+        todoApp,
+        persistedState
+    );
+
+    store.subscribe(throttle(() => {
+        saveState({
+            todos: store.getState().todos,
+        });
+    }, 1000));
+    return store;
+
+};
+
+export default configureStore;
